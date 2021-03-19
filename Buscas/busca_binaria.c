77fe7292fbfa1a5a52d627931e65f692d72c7857
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2021-03-19 01:11:13 -0300

    Busca binária iterativa

--- Buscas/busca_binaria.c
+++ Buscas/busca_binaria.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 /** ************************************ */
+int binarySearch(int key, int begin, int end, int array[]);
+int recursiveBinarySearch(int key, int begin, int end, int array[]);
 
 int main () {
 
-    int array[] = {1, 2, 3}, tam;
+    int array[] = {1, 2, 3, 4, 5, 6, 7}, tam;
     tam = sizeof(array)/ sizeof(array[0]);
 
-    //int r = recursiveBinarySearch(2, 0, tam - 1, array);
-    //printf("%d\n", r);
+    int r = recursiveBinarySearch(7, 0, tam - 1, array);
+    printf("%d\n", r);
 
     return 0;
 }
@@ -23,14 +25,29 @@ int binarySearch (int key, int begin, int end, int array[]) {
         if (key > array[middle])
             begin = middle + 1;
         else
-            if (key < array[middle])
-                end = middle - 1;
-            else
-                return middle;
+        if (key < array[middle])
+            end = middle - 1;
+        else
+            return middle;
 
     }
 
     return -1;
 }
 
+int recursiveBinarySearch (int key, int begin, int end, int array[]) {
+
+    int middle = (begin + end) / 2;
+
+    if (end < begin)
+        return -1;
+
+    if (key > array[middle])
+        recursiveBinarySearch(key, middle + 1, end, array);
+    else
+    if (key < array[middle])
+        recursiveBinarySearch(key, begin, middle - 1, array);
+    else
+        return middle;
+}
 
